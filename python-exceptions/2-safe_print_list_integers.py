0347896605ad6cd81dc79616fe0655b0b8fdc7d8
Nicolaslegoat <n.mancino03@gmail.com>
2023-10-06 13:25:46 -0700

    add file

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    le = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            le += 1
+        except (TypeError, ValueError):
+            continue
+    print()
+    return (le)
