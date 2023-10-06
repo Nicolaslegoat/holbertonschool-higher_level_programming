902f6f16ebc433a4c80c2f46b53cc1d83dca95b9
Nicolaslegoat <n.mancino03@gmail.com>
2023-10-06 11:26:51 -0700

    pycodestyle

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in my_list:
             if le < x:
-                print("{}".format(i), end ="")
+                print("{}".format(i), end="")
                 le += 1
         print()
         return (le)
