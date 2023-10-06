d23a381a1f65fda669cc29cd18add21afca5f0f7
Nicolaslegoat <n.mancino03@gmail.com>
2023-10-06 11:23:58 -0700

    add file

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    le = 0
+    try:
+        for i in my_list:
+            if le < x:
+                print("{}".format(i), end ="")
+                le += 1
+        print()
+        return (le)
+    except IndexError:
+        print()
