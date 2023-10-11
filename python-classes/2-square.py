85fe270e7a2e02475397ad169ba246c3bd123a08
Nicolaslegoat <n.mancino03@gmail.com>
2023-10-11 04:46:32 -0700

    change equation location

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -3,9 +3,10 @@
 
 
 class Square:
+    """empty class"""
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        self.valeur = size
         if size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
