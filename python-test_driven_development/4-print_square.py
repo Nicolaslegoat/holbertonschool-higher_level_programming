d3938c575915659e51de8fba5ab513d73d04d510
Nicolaslegoat <n.mancino03@gmail.com>
2023-10-13 03:54:36 -0700

    update file

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -3,9 +3,10 @@
 
 
 def print_square(size):
+    """a function print a square"""
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    for i in range(size)
+    for i in range(size):
         print(size * '#')
