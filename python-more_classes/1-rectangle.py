133a1e050063fd65ad2945a8d5aa0fc47d65491f
Nicolaslegoat <n.mancino03@gmail.com>
2023-10-13 07:16:08 -0700

    add file ok

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""empty module"""
+
+
+class Rectangle:
+    """a class create rectangle"""
+    def __init__(self, width=0, height=0):
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be an integer")
+        self.__height = height
+
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be an integer")
+        self.__width = width
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be an integer")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("heught must be an integer")
+        if value < 0:
+            raise ValueError("height must be an integer")
+        self.__height = value
