65e1d1554a00171085fea289dc4c36c500c938d4
Nicolaslegoat <n.mancino03@gmail.com>
2023-10-18 15:47:09 -0700

    add file

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Geometry module"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """That class represent a square"""
+    def __init__(self, size):
+        """This initialize a new square"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Calculate area of the square"""
+        return self.__size ** 2
+    
+    def __str__(self):
+        string = "[Square] "
+        string += str(self.__size) + "/" + str(self.__size)
+        return string
+    
