813c17d9c7bf988ae1641baa8fe325c086dfd664
Nicolaslegoat <n.mancino03@gmail.com>
2023-10-19 10:06:16 -0700

    add file ok

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Read module"""
+
+
 def read_file(filename=""):
+    """read and prints the content of a text file"""
     with open(filename, 'r', encoding="utf-8") as file:
         read_data = file.read()
     print(read_data)
