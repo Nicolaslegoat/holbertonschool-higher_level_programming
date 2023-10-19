472a1f55457864313717e44d698931a803fb7040
Nicolaslegoat <n.mancino03@gmail.com>
2023-10-19 10:08:25 -0700

    not return line

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """read and prints the content of a text file"""
     with open(filename, 'r', encoding="utf-8") as file:
         read_data = file.read()
-    print(read_data)
+    print(read_data, end="")
