54d00d3a09885115d059dffefa2043b77033715a
Nicolaslegoat <n.mancino03@gmail.com>
2023-10-20 04:42:10 -0700

    add file

---
+++ python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""module JSON"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """initialize a new Student object"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            result = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    result[attr] = getattr(self, attr)
+            return result
