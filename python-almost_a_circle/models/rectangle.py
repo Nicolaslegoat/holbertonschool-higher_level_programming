ebca80d6dbb22c1aa1ca0b86736be42a03dd00da
Nicolaslegoat <n.mancino03@gmail.com>
2023-10-23 14:53:03 -0700

    add file ok

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -100,3 +100,7 @@ class Rectangle(Base):
         """a method that print the rectangle"""
         for i in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        return "[Rectangle] ({}) {}/{} - {}/{}"\
+            .format(self.id, self.__x, self.__y, self.__width, self.__height)
