821455c9c53cbd0b7b2bfd6c75b6ec6b2b794245
Nicolaslegoat <n.mancino03@gmail.com>
2023-10-23 12:49:38 -0700

    add comm

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -3,6 +3,7 @@
 from models.base import Base
 
 class Rectangle(Base):
+    """The rectangle function"""
     def __init__(self, width, height, x=0, y=0, id = None):
         super().__init__(id)
         self.width = width
