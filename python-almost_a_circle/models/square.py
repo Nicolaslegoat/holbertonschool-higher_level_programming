cdef6ae2838c252ad0dc9ad9a83eb0366b8fa156
Nicolaslegoat <n.mancino03@gmail.com>
2023-10-24 01:03:53 -0700

    return and not print

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -34,5 +34,5 @@ class Square(Rectangle):
         """
         That return a representation of the square
         """
-        print("[Square] ({}) {}/{} - {}"
+        return ("[Square] ({}) {}/{} - {}"
               .format(self.id, self.x, self.y, self.size))
