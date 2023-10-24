4808d7f3c2bd86803158a5880403fdfe8e495d76
Nicolaslegoat <n.mancino03@gmail.com>
2023-10-24 01:01:46 -0700

    add file ok

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+This module defines the rectangle and square class
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class Square inherits from Rectangle
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initializes a new Square instance.
+        """
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """
+        That retrieves the size of the square sides
+        """
+        return self.height
+
+    @size.setter
+    def size(self, value):
+        """
+        Method to defines the size of the square sides
+        """
+        self.height = value
+        self.width = value
+
+    def __str__(self):
+        """
+        That return a representation of the square
+        """
+        print("[Square] ({}) {}/{} - {}"
+              .format(self.id, self.x, self.y, self.size))
