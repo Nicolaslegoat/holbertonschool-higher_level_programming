6780b94ec06ae4512d9c5eb4c663b66746b6149e
Nicolaslegoat <n.mancino03@gmail.com>
2023-10-24 02:12:33 -0700

    add file ok

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -36,3 +36,29 @@ class Square(Rectangle):
         """
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x,
                                                  self.y, self.size)
+
+    def update(self, *args, **kwargs):
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.size = kwargs["size"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
+
+    def to_dictionnary(self):
+        return {"id": self.id,
+                "size": self.size,
+                "x": self.x,
+                "y": self.y}
