fc7b3efb86e2de71fec240033ffc839fab8bf4c2
Nicolaslegoat <n.mancino03@gmail.com>
2023-10-26 09:26:40 -0700

    pycode

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -32,7 +32,4 @@ class Base:
             dict_list = [obj.to_dictionary() for obj in list_objs]
             json_string = cls.to_json_string(dict_list)
             file.write(json_string)
-
-
-
         
